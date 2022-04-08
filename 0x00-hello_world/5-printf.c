93d67c1537c88e81851b5cef76d720d77dabd7c6
Haywhiz <babarindeademola@gmail.com>
2022-04-08 04:06:51 -0700

    c_task6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  * function - printf
- * return: always 0 (success)
+ * return: Always 0 (success)
 */
 int main(void)
-{	
+{
 	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
-}	
+}
