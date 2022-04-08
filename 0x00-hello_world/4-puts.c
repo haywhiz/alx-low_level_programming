bb15162659aecee303c64c89630f0baaddbb2ef5
Haywhiz <babarindeademola@gmail.com>
2022-04-08 03:35:01 -0700

    c_task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Entry point
  * use function puts
  * Return: Always 0 (Success)
-*/ 
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
