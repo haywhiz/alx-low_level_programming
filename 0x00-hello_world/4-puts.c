c6e2150294cc375d9e1958c05786aba0cee812fb
Haywhiz <babarindeademola@gmail.com>
2022-04-08 03:15:25 -0700

    c_task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-/** function to display by 
-* using puts
-*/ 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *   * Return: Always 0 (Success)
+ *
+ *     */ 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");	
+return (0);
 }
