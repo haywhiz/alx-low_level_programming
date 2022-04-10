0391eea013dd4a6fdd58169977b8599dac4adf24
Haywhiz <babarindeademola@gmail.com>
2022-04-10 14:36:48 -0700

    loop_task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - using break and continue op
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
