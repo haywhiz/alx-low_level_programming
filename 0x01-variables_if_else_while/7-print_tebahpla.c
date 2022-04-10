988e24c9252d9584214c25892c64c4af155830ef
Haywhiz <babarindeademola@gmail.com>
2022-04-10 16:27:07 -0700

    loop_task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - program to write lowercase alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c >= 'z'; c--)
+	{
+		putchar(c);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
+
+
