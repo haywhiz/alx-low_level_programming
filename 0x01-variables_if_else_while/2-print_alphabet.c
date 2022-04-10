d4bc21079d552981087757d6f86a417feee46981
Haywhiz <babarindeademola@gmail.com>
2022-04-10 11:04:26 -0700

    loop_task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - program to write lowercase alphabet
+ * Return: Always 0 (success)
+ */
+int main()
+{
+	char c = c;
+	for (c = 'a'; c >= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
+
+
+
