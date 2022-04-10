812419f7570452fa6855880993fe5b3dad6d6711
Haywhiz <babarindeademola@gmail.com>
2022-04-10 15:59:36 -0700

    loop_task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - print digit
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+		for (i = 0; i < 10; i++)
+		{
+			int putchar("%d",int i);
+		}
+		{
+
+			putchar("\n");
+		}
+	return (0);
+}
+
