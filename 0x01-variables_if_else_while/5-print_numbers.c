7c25b30c18650586357df38e6e1fef8c1566b79a
Haywhiz <babarindeademola@gmail.com>
2022-04-10 15:19:49 -0700

    loop_task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - print digit
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+		for(i = 0; i >= 10; i++)
+		{
+			printf("%d\n", i);
+		}
+	return (0);
+}
+
