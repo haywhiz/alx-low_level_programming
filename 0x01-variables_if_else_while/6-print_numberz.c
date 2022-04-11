f5993206c932e2f78638a94aa943ed96fc523851
Haywhiz <babarindeademola@gmail.com>
2022-04-11 05:17:20 -0700

    loop_task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,13 @@
 int main(void)
 {
 	int i;
-       for (i = 48; i <= 57; i++)
-		{
-			putchar(i);	
-		}
-		{
-
-			putchar('\n');
-		}
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
 	return (0);
 }
 
