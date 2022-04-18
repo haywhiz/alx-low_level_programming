e2a92057c626835ee6a0d8d496d47caaaaa41542
Haywhiz <babarindeademola@gmail.com>
2022-04-18 09:11:27 -0700

    debugging_update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,40 +9,24 @@
 */
 
 
-int largest_number(int a, int b, int c)
-
+int largest_number(int a, int b, int c);
 {
+	int largest;
+	if (a >= b && b >= c)
+	{
+		largest = a;
+	}
+		else if (b >= a && a >= c)
+		{
+			largest = b;
+		}
+		else if (c >= a && c >= b)
+		{
+			largest = c;
+		}
+	else
+		printf("%lu", int largest_number);
+	
 
-	    int largest;
-
-
-
-	        if (a > b && b > c)
-
-			    {
-
-				            largest = a;
-
-					        }
-
-		    else if (b > a && a > c)
-
-			        {
-
-					        largest = b;
-
-						    }
-
-		        else
-
-				    {
-
-					            largest = c;
-
-						        }
-
-
-
-			    return (largest);
-
+	return (largest);
 }
