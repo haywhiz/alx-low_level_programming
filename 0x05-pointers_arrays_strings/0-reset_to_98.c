e44d77682c057037a05dc9e7adc0fdf603636728
Haywhiz <babarindeademola@gmail.com>
2022-04-20 15:09:45 -0700

    pointers_001

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description - pointers task
+ * description - function reset_to_98
+ * Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	int n;
+	int p;
+
+	n = 98;
+	*p = &n;
+
+	_putchar("%d\n", n);
+
+	return (0);
+}
+
