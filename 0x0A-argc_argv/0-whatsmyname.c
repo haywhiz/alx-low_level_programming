a532060ade87f82d0f8214d302895d9d03ac588a
Haywhiz <babarindeademola@gmail.com>
2022-05-04 08:55:27 -0700

    argc_argv00

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *@argc: Counts the number of parameters that go into main
+ *@argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
